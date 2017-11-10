f82516e506633e9dc571eeb4ac3aabb149f6d6be
jose-lopez <josesmooth@gmail.com>
2017-11-10 11:47:35 -0430

    ajustando

--- src/clasificador/Clasificador.java
+++ src/clasificador/Clasificador.java
@@ -5,70 +5,195 @@
  */
 package clasificador;
 
-
 import archivos.LeerArchivo;
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import javax.swing.JFileChooser;
 
 /**
  *
  * @author Liquid
  */
-
-
 public class Clasificador {
-    
-public static ClasificadorADN Ocla;
-File DatosTxt;
-public static int modelo,gen;
-public static int [] positivos;
-    
-public static void main(String [] args) throws Exception{
-    String RutaModelo=null, RutaData;
-    boolean repeat=true;
-    BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
-    Ocla = new ClasificadorADN();
-    
-    while(repeat){
-        try{
-            System.out.println("Seleccione Gen a identificar:");
-            System.out.println("0. Exon-Intron (GT)");
-            System.out.println("1. Intron-Exon (AG)");
-            gen = Integer.parseInt(reader.readLine());
 
-            System.out.println("Seleccione modelo a emplear:");
-            System.out.println("0. Conjunctive Rule ");
-            System.out.println("1. Multilayer Perceptron");
-            System.out.println("2. Tree J48");
-            modelo = Integer.parseInt(reader.readLine());
-            if(gen > 1 || modelo > 2)repeat = true;
-            else repeat=false;
-        }catch(Exception e){
-            repeat = true;            
-        }
+    public ClasificadorADN oCla;    
+    File DatosTxt;
+    public int modelo, sitio;
+    public int[] positivos, positivos2;
+    public String rutaSecuencia;
+
+    public void setoCla(ClasificadorADN oCla) {
+        this.oCla = oCla;
     }
+
+    public void setPositivos(int[] positivos) {
+        this.positivos = positivos;
+    }
+
+    public ClasificadorADN getoCla() {
+        return oCla;
+    }
+
+    public int getModelo() {
+        return modelo;
+    }
+
+    public int getSitio() {
+        return sitio;
+    }
+
+    public int[] getPositivos() {
+        return positivos;
+    }
+
+    public String getRutaSecuencia() {
+        return rutaSecuencia;
+    }
+   
+    public void setRutaSecuencia(String rutaSecuencia) {
+        this.rutaSecuencia = rutaSecuencia;
+    }
+
+    public void setModelo(int modelo) {
+        this.modelo = modelo;
+    }
+
+    public void setSitio(int sitio) {
+        this.sitio = sitio;
+    }
+
+    public Clasificador(int sitio, int modelo, String rutaSecuencia) {
         
+        setModelo(modelo);
+        setSitio(sitio);
+        oCla = new ClasificadorADN();
+        setRutaSecuencia(rutaSecuencia);       
+        
+    }
+
+    public static void main(String[] args) throws Exception {
+        String RutaModelo = null, RutaData;
+       
+        int modelo, sitio;
+        
+        RutaData = args[2];
+        sitio = Integer.parseInt(args[0]);
+        modelo = Integer.parseInt(args[1]);
+        
+        ClasificadorADN oCla = new ClasificadorADN();
+        /*
+        while (repeat) {
+            try {
+                System.out.println("Seleccione Gen a identificar:");
+                System.out.println("0. Exon-Intron (GT)");
+                System.out.println("1. Intron-Exon (AG)");
+                sitio = Integer.parseInt(reader.readLine());
+
+                System.out.println("Seleccione modelo a emplear:");
+                System.out.println("0. Conjunctive Rule ");
+                System.out.println("1. Multilayer Perceptron");
+                System.out.println("2. Tree J48");
+                modelo = Integer.parseInt(reader.readLine());
+                if (sitio > 1 || modelo > 2) {
+                    repeat = true;
+                } else {
+                    repeat = false;
+                }
+            } catch (Exception e) {
+                repeat = true;
+
+            }
+        }*/
+
+        if (modelo == 0 && sitio == 0) {
+            //home/jose/NetBeansProjects/clasificadorGTAG/EI_GT/4Modelos
+            RutaModelo = "EI_GT/4Modelos/ConjunctiveRule.model";
+        }
+        if (modelo == 1 && sitio == 0) {
+            RutaModelo = "EI_GT/4Modelos/MultiLayerPerceptronAS.model";
+        }
+        if (modelo == 2 && sitio == 0) {
+            RutaModelo = "EI_GT/4Modelos/TreeJ48.model";
+        }
+
+        if (modelo == 0 && sitio == 1) {
+            RutaModelo = "IE_AG/4Modelos/ConjunctiveRule.model";
+        }
+        if (modelo == 1 && sitio == 1) {
+            RutaModelo = "IE_AG/4Modelos/MultiLayerPerceptronAS.model";
+        }
+        if (modelo == 2 && sitio == 1) {
+            RutaModelo = "IE_AG/4Modelos/TreeJ48.model";
+        }
+
+        //RutaData = "test/gen_SST.txt";
+
+        int[] positivos;
+        File datos = new File(RutaData);
+        positivos = oCla.ClasificarTxt(datos, modelo, sitio, RutaModelo, true);
+
+        System.out.println("Vector de positivos: " + Arrays.toString(positivos));
+
+        //positivos2 = new Clasificador().clasificador(modelo, sitio, "test/gen_SST.txt");
+
+        System.out.println("FIN DEL PROCESO");
+
+    } 
+
+    public List<Integer> clasificador() throws Exception {
+
+        String rutaModelo;
+        //oCla = new ClasificadorADN();
+        /*
+         if(modelo == 0 && Clasificador.sitio == 0)rutaModelo="EI_GT/4Modelos/ConjunctiveRule.model";
+         if(modelo == 1 && Clasificador.sitio == 0)rutaModelo="EI_GT/4Modelos/MultiLayerPerceptron.model";
+         if(modelo == 2 && Clasificador.sitio == 0)rutaModelo="EI_GT/4Modelos/TreeJ48.model";
     
-    
-    
-    if(modelo == 0 && gen == 0)RutaModelo="EI_GT/4Modelos/ConjunctiveRule.model";
-    if(modelo == 1 && gen == 0)RutaModelo="EI_GT/4Modelos/MultiLayerPerceptron.model";
-    if(modelo == 2 && gen == 0)RutaModelo="EI_GT/4Modelos/TreeJ48.model";
-    
-    if(modelo == 0 && gen == 1)RutaModelo="IE_AG/4Modelos/ConjunctiveRule.model";
-    if(modelo == 1 && gen == 1)RutaModelo="IE_AG/4Modelos/MultiLayerPerceptron.model";
-    if(modelo == 2 && gen == 1)RutaModelo="IE_AG/4Modelos/TreeJ48.model";
-    
-    RutaData="test/gen_SST.txt";
-    
-    
-    File datos = new File(RutaData);
-    positivos = Ocla.ClasificarTxt(datos, modelo, gen,RutaModelo);
-    
-    System.out.println("Vector de positivos: "+ Arrays.toString(positivos));
-    
-    System.out.println("FIN DEL PROCESO");
-    
-}    
+         if(modelo == 0 && Clasificador.sitio == 1)rutaModelo="IE_AG/4Modelos/ConjunctiveRule.model";
+         if(modelo == 1 && Clasificador.sitio == 1)rutaModelo="IE_AG/4Modelos/MultiLayerPerceptron.model";
+         if(modelo == 2 && Clasificador.sitio == 1)rutaModelo="IE_AG/4Modelos/TreeJ48.model";
+         * */
+        rutaModelo = "";
+
+        if (modelo == 0 && sitio == 0) {
+            rutaModelo = "clasificador/modelosGT/ConjunctiveRule.model";
+        }
+        if (modelo == 1 && sitio == 0) {
+            rutaModelo = "clasificador/modelosGT/MultiLayerPerceptronAS.model";
+        }
+        if (modelo == 2 && sitio == 0) {
+            rutaModelo = "clasificador/modelosGT/TreeJ48.model";
+        }
+
+        if (modelo == 0 && sitio == 1) {
+            rutaModelo = "clasificador/modelosAG/ConjunctiveRule.model";
+        }
+        if (modelo == 1 && sitio == 1) {
+            rutaModelo = "clasificador/modelosAG/MultiLayerPerceptronAS.model";
+        }
+        if (modelo == 2 && sitio == 1) {
+            rutaModelo = "clasificador/modelosAG/TreeJ48.model";
+        }
+
+        File datos = new File(rutaSecuencia);         
+        
+        positivos = oCla.ClasificarTxt(datos, modelo, sitio, rutaModelo, true);
+
+        System.out.println("Vector de positivos: " + Arrays.toString(positivos));
+
+        System.out.println("FIN DEL PROCESO");
+        
+        List<Integer> coords = new ArrayList<>();
+        
+        for(int i = 0; i < positivos.length; i++){
+            
+            coords.add(i, positivos[i]);
+            
+        }
+
+        return coords;
+
+    }
 }
--- src/clasificador/ClasificadorADN.java
+++ src/clasificador/ClasificadorADN.java
@@ -37,82 +37,113 @@ public class ClasificadorADN {
     File Carpeta;
     Instances datapredict;
     Instances predicteddata;
-    String[] attributeNames; 
-    FastVector atts; 
+    String[] attributeNames;
+    FastVector atts;
     Integer[] posiciones;
-    String TextoModelo="",TextoGen="";
+    String TextoModelo = "", TextoGen = "";
     Attribute ClassAttribute;
-    int [] positivos;
+    int[] positivos;
     int detectados;
-    
-    public ClasificadorADN(){
-        
+
+    public ClasificadorADN() {
     }
-    public void CargarModelo(String ruta, int modelo) throws Exception{
-        
-        switch (modelo){
+
+    public void CargarModelo(String ruta, int modelo) throws Exception {
+
+        String path = System.getProperty("user.dir");
+        ruta = path + "/" + ruta;
+
+        switch (modelo) {
             case 0:
                 cr = (ConjunctiveRule) weka.core.SerializationHelper.read(ruta);
                 //cr.buildClassifier(datapredict);
-                TextoModelo="ConjunctiveRule-";
-                
+                TextoModelo = "ConjunctiveRule-";
+
                 break;
             case 1:
                 mlp = (MultilayerPerceptron) weka.core.SerializationHelper.read(ruta);
-                TextoModelo="MultiLayerPerceptron-";
+                TextoModelo = "MultiLayerPerceptron-";
                 break;
             case 2:
                 tree = (J48) weka.core.SerializationHelper.read(ruta);
-                TextoModelo="TreeJ48-";
+                TextoModelo = "TreeJ48-";
                 break;
         }
         System.out.println("Modelo Cargado");
     }
-    public void CargarData(String ruta) throws Exception{
+
+    public void CargarData(String ruta) throws Exception {
         datapredict = new Instances(
-                            new BufferedReader(new FileReader(ruta)));
+                new BufferedReader(new FileReader(ruta)));
         datapredict.setClassIndex(datapredict.numAttributes() - 1);
         predicteddata = new Instances(datapredict);
     }
-    public void CrearInstancias(int gen){
+
+    public void CrearInstancias(int gen) {
+
         atts = new FastVector(11);
 //        attributeNames = new String[]{"B1","B2","B3","B4","B5","B6","B7","B8","B9","B10","CLASS"};
 //        atts = new FastVector();
 //        Arrays.stream(attributeNames).map(Attribute::new).forEach(atts::addElement);
 //       
-        
-        
-       
-       for (int i=0;i<10;i++){
-           atts.addElement(new Attribute("B"+(i+1)));
-       } 
-       if(gen==0){
-           FastVector fvClassVal = new FastVector(2);
-           fvClassVal.addElement("E-");
-           fvClassVal.addElement("E+");
-           ClassAttribute = new Attribute("CLASS", fvClassVal);
-       }
-       if(gen==1){
-           FastVector fvClassVal = new FastVector(2);
-           fvClassVal.addElement("I-");
-           fvClassVal.addElement("I+");
-           ClassAttribute = new Attribute("CLASS", fvClassVal);
-       }
-       atts.addElement(ClassAttribute);
-       
+
+
+
+        for (int i = 0; i < 10; i++) {
+            atts.addElement(new Attribute("B" + (i + 1)));
+        }
+        if (gen == 0) {
+            FastVector fvClassVal = new FastVector(2);
+            fvClassVal.addElement("E-");
+            fvClassVal.addElement("E+");
+            ClassAttribute = new Attribute("CLASS", fvClassVal);
+        }
+        if (gen == 1) {
+            FastVector fvClassVal = new FastVector(2);
+            fvClassVal.addElement("I-");
+            fvClassVal.addElement("I+");
+            ClassAttribute = new Attribute("CLASS", fvClassVal);
+        }
+        atts.addElement(ClassAttribute);
+
     }
-    public void SeleccionarCarpeta(File car)throws Exception{
+
+    public void crearAtributos(int gen, int canAtrib) {
+
+        atts = new FastVector(canAtrib);
+
+        for (int i = 0; i < canAtrib - 1; i++) {
+            atts.addElement(new Attribute("B" + (i + 1)));
+        }
+        if (gen == 0) {
+            FastVector fvClassVal = new FastVector(2);
+            fvClassVal.addElement("E-");
+            fvClassVal.addElement("E+");
+            ClassAttribute = new Attribute("CLASS", fvClassVal);
+        }
+        if (gen == 1) {
+            FastVector fvClassVal = new FastVector(2);
+            fvClassVal.addElement("I-");
+            fvClassVal.addElement("I+");
+            ClassAttribute = new Attribute("CLASS", fvClassVal);
+        }
+        atts.addElement(ClassAttribute);
+
+    }
+
+    public void SeleccionarCarpeta(File car) throws Exception {
         Carpeta = car;
         Carpeta.createNewFile();
     }
-    public void Clasificar(int modelo)throws Exception{
-        double clsLabel=0;
-        detectados=0;
+
+    public void Clasificar(int modelo) throws Exception {
+        double clsLabel = 0;
+        detectados = 0;
         for (int i = 0; i < datapredict.numInstances(); i++) {
-            
+
             //System.out.println(datapredict.instance(i).toString());
-            
-            switch (modelo){
+
+            switch (modelo) {
                 case (0):
                     clsLabel = cr.classifyInstance(datapredict.instance(i));
                     break;
@@ -123,63 +154,78 @@ public class ClasificadorADN {
                     clsLabel = tree.classifyInstance(datapredict.instance(i));
                     break;
             }
-            if(clsLabel==1)detectados++;
-            System.out.println("marcador "+i+" respuesta: "+clsLabel);            
+            if (clsLabel == 1) {
+                detectados++;
+            }
+            System.out.println("marcador " + i + " respuesta: " + clsLabel);
             predicteddata.instance(i).setClassValue(clsLabel);
-            
-           
+
+
         }
-        
-        positivos = new int [detectados];
-        int k=0;
+
+        positivos = new int[detectados];
+        int k = 0;
         for (int j = 0; j < predicteddata.numInstances(); j++) {
-            if(predicteddata.instance(j).classValue()==1){
-                positivos[k]=posiciones[j];
+            if (predicteddata.instance(j).classValue() == 1) {
+                positivos[k] = posiciones[j];
                 k++;
             }
         }
         System.out.println("Datos Clasificados");
     }
-    public int [] ClasificarTxt(File datos, int modelo, int gen, String RutaModelo) throws Exception{
-        String genstr="",genstrclean="";
-            switch (gen){
-                case 0:
-                    genstr="g,t";
-                    genstrclean="gt";
-                    TextoGen="Exon-Intron-GT-";
-                    break;
-                case 1:
-                    genstr="a,g";
-                    genstrclean="ag";
-                    TextoGen="Intron-Exon-AG-";
-                    break;
-            }
-            LeerArchivo arcp = new LeerArchivo(datos.getPath());
-         
-            int lineas = arcp.CantidadOcurrencias(genstr);
+
+    public int[] ClasificarTxt(File datos, int modelo, int gen, String RutaModelo, boolean modeloAlternativo) throws Exception {
+        String genstr = "", genstrclean = "";
+        switch (gen) {
+            case 0:
+                genstr = "g,t";
+                genstrclean = "gt";
+                TextoGen = "Exon-Intron-GT-";
+                break;
+            case 1:
+                genstr = "a,g";
+                genstrclean = "ag";
+                TextoGen = "Intron-Exon-AG-";
+                break;
+        }
+        LeerArchivo arcp = new LeerArchivo(datos.getPath());
+
+        int lineas = arcp.CantidadOcurrencias(genstr);
+
+        if (!modeloAlternativo) {
             CrearInstancias(gen);
-            datapredict = new Instances("data", atts, lineas);
-            
-            
-            
-            //String[] Data = new String[lineas];
-            posiciones = new Integer[lineas];
-                       
-            
-            
-            int ConPos = 0;
-            int CanCar = 0;
-            int contador=0;
-            String linea = arcp.LeerLinea();
-            String captura;
-            linea = linea.replace("[", "");
-            linea = linea.replace("]", "");
-            linea = linea.replace(",", "");
-            for (int i = -1; (i = linea.indexOf(genstrclean, i + 1)) != -1; ) {
-                captura="";
-                try{
-                    captura = linea.substring(i-5, i+7);
-                    captura = captura.replace(genstrclean, "");
+        } else {
+            if (gen == 0) {
+                crearAtributos(gen, 7);
+            } else {
+                crearAtributos(gen, 5);
+            }
+        }
+
+        datapredict = new Instances("data", atts, lineas);
+        //String[] Data = new String[lineas];
+        posiciones = new Integer[lineas];
+
+        int ConPos = 0;
+        int CanCar = 0;
+        int contador = 0;
+        int longLinea, limInf, limSup;
+        String linea = arcp.LeerLinea();
+        String captura;
+        linea = linea.replace("[", "");
+        linea = linea.replace("]", "");
+        linea = linea.replace(",", "");
+        longLinea = linea.length();
+        for (int i = -1; (i = linea.indexOf(genstrclean, i + 1)) != -1;) {
+            captura = "";
+            try {
+                limInf = i - 5;
+                limSup = i + 7;
+
+                if (limInf > 0 && limSup < longLinea) {
+
+                    captura = linea.substring(limInf, i);
+                    captura = captura + linea.substring(i + 2, limSup);
                     //System.out.println("Ocurrencia "+contador+" gen "+captura+" en "+i);         
                     contador++;
                     captura = captura.replace("a", "0");
@@ -189,52 +235,81 @@ public class ClasificadorADN {
 
                     String[] bases = captura.split("");
 
+                    int canAtrib = datapredict.numAttributes();
+                    double[] attValues = new double[canAtrib];
 
+                    if (modeloAlternativo) {
+                        if (gen == 0) {
+                            attValues[0] = Integer.parseInt(bases[3]);
+                            attValues[1] = Integer.parseInt(bases[5]);
+                            attValues[2] = Integer.parseInt(bases[6]);
+                            attValues[2] = Integer.parseInt(bases[7]);
+                            attValues[4] = Integer.parseInt(bases[8]);
+                            attValues[5] = Integer.parseInt(bases[10]);
+                            attValues[canAtrib - 1] = datapredict.attribute(canAtrib - 1).addStringValue("?");
+                        } else {
+                            attValues[0] = Integer.parseInt(bases[1]);
+                            attValues[1] = Integer.parseInt(bases[3]);
+                            attValues[2] = Integer.parseInt(bases[4]);
+                            attValues[3] = Integer.parseInt(bases[5]);
+                            attValues[canAtrib - 1] = datapredict.attribute(canAtrib - 1).addStringValue("?");
+                        }
+                    } else {
+                        for (int j = 0; j < canAtrib - 1; j++) {
 
-                    double[] attValues = new double[datapredict.numAttributes()];         
+                            attValues[j] = Integer.parseInt(bases[j + 1]);
 
-
-                    for (int j = 0; j < 10; j++) {
-
-                        attValues[j] = Integer.parseInt(bases[j]);
+                        }
+                        attValues[canAtrib - 1] = datapredict.attribute(canAtrib - 1).addStringValue("?");
                     }
-                    attValues[10] = datapredict.attribute(10).addStringValue("?");
-
 
                     datapredict.add(new Instance(1.0, attValues));
 
                     posiciones[ConPos] = i;
-                    ConPos++;            
-                }catch(StringIndexOutOfBoundsException e){}
-                
+                    ConPos++;
+                }
+            } catch (StringIndexOutOfBoundsException e) {
             }
-            
-            datapredict.setClassIndex(datapredict.numAttributes()-1);
-            CargarModelo(RutaModelo, modelo);
-            predicteddata = new Instances(datapredict);
-            Clasificar(modelo);
-            GenerarArff(true);
-            return(positivos);
+
+        }
+
+        datapredict.setClassIndex(datapredict.numAttributes() - 1);
+        CargarModelo(RutaModelo, modelo);
+        predicteddata = new Instances(datapredict);
+        Clasificar(modelo);
+        GenerarArff(true);
+
+        if (gen == 1) {
+
+            for (int pos = 0; pos < positivos.length; pos++) {
+
+                positivos[pos] = positivos[pos] + 1;
+
+            }
+
+        }
+
+        return (positivos);
     }
-    
-    public void GenerarArff(boolean pos)throws Exception{
+
+    public void GenerarArff(boolean pos) throws Exception {
         EscribirArchivo arc;
         String timeStamp = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss").format(Calendar.getInstance().getTime());
-        String RutaResultado="";
-        RutaResultado="results/Resultado-"+TextoGen+TextoModelo+timeStamp+".txt";
-        System.out.println("Resultados Generados en: "+RutaResultado);
+        String RutaResultado = "";
+        RutaResultado = "results/Resultado-" + TextoGen + TextoModelo + timeStamp + ".txt";
+        System.out.println("Resultados Generados en: " + RutaResultado);
         arc = new EscribirArchivo(RutaResultado, true);
         if (pos) {
             for (int i = 0; i < datapredict.numInstances(); i++) {
-                arc.EscribirEnArchivo(posiciones[i]+":"+predicteddata.instance(i).toString());
+                arc.EscribirEnArchivo(posiciones[i] + ":" + predicteddata.instance(i).toString());
             }
         } else {
             for (int i = 0; i < datapredict.numInstances(); i++) {
                 arc.EscribirEnArchivo(predicteddata.instance(i).toString());
             }
         }
-        arc.EscribirEnArchivo("Positivos Encontrados: "+Arrays.toString(positivos));
-        System.out.println("Cantidad de positivos encontrados: "+detectados);
-       // System.out.println(Arrays.toString(positivos));
+        arc.EscribirEnArchivo("Positivos Encontrados: " + Arrays.toString(positivos));
+        System.out.println("Cantidad de positivos encontrados: " + detectados);
+        // System.out.println(Arrays.toString(positivos));
     }
 }
