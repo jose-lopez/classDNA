c8e9ca88d9e60494964877924b221046a9132866
jose-lopez <josesmooth@gmail.com>
2019-07-30 19:49:34 -0400

    Modelado con datos nuevos para las cuatro zonas.

--- src/clasificador/Clasificador.java
+++ src/clasificador/Clasificador.java
@@ -149,7 +149,8 @@ public class Clasificador {
         if (modelo == 1 && sitio == 0) {// ancho 5, 5
             rutaModelo = "EI_GT/4Modelos/MultiLayerPerceptron.model";
             seleccionAtributos=true;
-            vectorAtributos=new int[]{2,4,5,7}; 
+            vectorAtributos=new int[]{2,4,5,7};
+            //vectorAtributos=new int[]{4,5,6,7,9}; 
         }
         if (modelo == 2 && sitio == 0) {
             rutaModelo = "EI_GT/4Modelos/TreeJ48.model";
@@ -157,7 +158,7 @@ public class Clasificador {
         if (modelo == 3 && sitio == 0) {// ancho 5, 5
             rutaModelo = "EI_GT/4Modelos/BayesNet.model";
             seleccionAtributos=true;
-            vectorAtributos=new int[]{2,4,5,7}; 
+            vectorAtributos=new int[]{4,5,6,7}; // New Model Weka-5
         }
 
         if (modelo == 0 && sitio == 1) {
@@ -167,6 +168,8 @@ public class Clasificador {
             rutaModelo = "IE_AG/4Modelos/MultiLayerPerceptron.model";
             seleccionAtributos=true;
             vectorAtributos=new int[]{18,58,60,66,77,82,83,85,86,87,88,89,90,91,92,93,94,95,97,98,99};
+            //vectorAtributos=new int[]{72,80,82,83,85,86,87,88,89,90,91,92,93,94,95,96,98,99,100,101,102,103};
+            
         }
         if (modelo == 2 && sitio == 1) {
             rutaModelo = "IE_AG/4Modelos/TreeJ48.model";
@@ -174,7 +177,7 @@ public class Clasificador {
         if (modelo == 3 && sitio == 1) { // Ancho 100, 5
             rutaModelo = "IE_AG/4Modelos/BayesNet.model";
             seleccionAtributos=true;
-            vectorAtributos=new int[]{18,58,60,66,77,82,83,85,86,87,88,89,90,91,92,93,94,95,97,98,99};
+            vectorAtributos=new int[]{59,72,80,83,85,86,87,88,89,90,91,92,93,94,95,96,98,99,100,101,102,103};
         }
         
         if (modelo == 0 && sitio == 2) {
@@ -184,6 +187,7 @@ public class Clasificador {
             rutaModelo = "EZ/4Modelos/MultiLayerPerceptron.model";
             seleccionAtributos=true; 
             vectorAtributos=new int[]{14,32,33,50,51,52,67,132,138,139,153,221,228,232,235,247};
+            //vectorAtributos=new int[]{31,33,34,36,50,51,52,53,63,171,181,213,229,356,368,386,406};
         }
         if (modelo == 2 && sitio== 2) {
             rutaModelo = "EZ/4Modelos/TreeJ48.model";
@@ -191,7 +195,7 @@ public class Clasificador {
         if (modelo == 3 && sitio == 2) { // Ancho 50, 200
             rutaModelo = "EZ/4Modelos/BayesNet.model";
             seleccionAtributos=true; 
-            vectorAtributos=new int[]{14,32,33,50,51,52,67,132,138,139,153,221,228,232,235,247};
+            vectorAtributos=new int[]{31,33,34,36,50,51,52,53,63,122,139,143,171,181,214,229,356,368,386,406,466};
         }
 
         if (modelo == 0 && sitio == 3) {
@@ -201,6 +205,7 @@ public class Clasificador {
             rutaModelo = "ZE/4Modelos/MultilayerPerceptron.model";
             seleccionAtributos=true;
             vectorAtributos=new int[]{118,186,471,482,483,500,501,502,511,517,522,536,582,598,603,671};
+            //vectorAtributos=new int[]{11,329,483,495,499,501,502,503,518};
         }
         if (modelo == 2 && sitio== 3) {
             rutaModelo = "ZE/4Modelos/TreeJ48.model";
@@ -208,7 +213,7 @@ public class Clasificador {
         if (modelo == 3 && sitio == 3) {// Ancho 500, 200
             rutaModelo = "ZE/4Modelos/BayesNet.model";
             seleccionAtributos=true;
-            vectorAtributos=new int[]{118,186,471,482,483,500,501,502,511,517,522,536,582,598,603,671};
+            vectorAtributos=new int[]{329,483,495,501,502,503,518};
         }
         
         File datos = new File(rutaSecuencia);
